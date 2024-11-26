24b2bb3c932c49e739c7228effd656091b24060c
socratesomiliadis <s.omiliadis@gmail.com>
2024-11-27 01:19:13 +0200

    test fix

--- frontend/lib/services/song.ts
+++ frontend/lib/services/song.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const API_URL =
   typeof window === "undefined"
     ? process.env.BACKEND_API_HOST
-    : "https://spotify-uom-api.sohub.digital";
+    : document.location.protocol + "://" + document.location.hostname + ":8002";
 
 const throwAxiosError = (err: any) => {
   if (!!err.response) {
