79bfe95a806c379eb71e8731aab596bb59ba4c88
socratesomiliadis <s.omiliadis@gmail.com>
2024-11-27 02:16:33 +0200

    added https support

--- backend/main.go
+++ backend/main.go
@@ -357,6 +357,6 @@ func main() {
 		json.NewEncoder(w).Encode(favorites)
 	})
 
-	http.ListenAndServe("0.0.0.0:8002", router)
+	http.ListenAndServeTLS("0.0.0.0:8002", "certificate.crt", "private.key", router)
 
 }
